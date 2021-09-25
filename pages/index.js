9b570343ed7144944f1fcfe7185f6865bcf0cd5a
Andrey Safonov <a.so777@live.ca>
2021-09-25 08:18:36 -0700

    Syntax lint fix

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,26 @@
-import {useEffect, useRef} from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function HomePage() {
+  const viewer = useRef(null);
 
-    const viewer = useRef(null);
+  useEffect(() => {
+    import('@pdftron/webviewer').then(() => {
+      WebViewer(
+        {
+          path: '/webviewer/lib',
+          initialDoc: '/files/pdftron_about.pdf',
+        },
+        viewer.current
+      ).then((instance) => {
+        const { docViewer } = instance;
+        // you can now call WebViewer APIs here...
+      });
+    });
+  }, []);
 
-    useEffect(() => {
-      import('@pdftron/webviewer').then(() => {
-        WebViewer(
-          {
-            path: '/webviewer/lib',
-            initialDoc: '/files/pdftron_about.pdf',
-          },
-          viewer.current,
-        ).then((instance) => {
-            const { docViewer } = instance;
-            // you can now call WebViewer APIs here...
-          });
-      })
-    }, []);
-
-
-
-    return (
-      <div className="MyComponent">
-        <div className="header">React sample</div>
-        <div className="webviewer" ref={viewer} style={{height: "100vh"}}></div>
-      </div>
-    );
-  
+  return (
+    <div className='MyComponent'>
+      <div className='webviewer' ref={viewer} style={{ height: '100vh' }}></div>
+    </div>
+  );
 }
